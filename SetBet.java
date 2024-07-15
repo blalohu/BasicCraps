fd26e9e878c75a32128aa1958265f339a2a9f7be
blalohu <117615040+blalohu@users.noreply.github.com>
2024-07-15 16:25:38 -0400

    added boolean on to constructor parameters

--- SetBet.java
+++ SetBet.java
@@ -3,8 +3,9 @@ public class SetBet extends PayBet{
     static boolean hornBetCheck;
 
 
-    SetBet(int bet) {
-        this.bet = bet;
+    SetBet(int bet, boolean on) {
+       this.on = on; 
+this.bet = bet;
         while (hornBetCheck && bet % 4 != 0) {
             System.out.println("Horn bets must be in multiples of 4, please try again.");
             bet = Craps.crapsScanner.nextInt();
@@ -14,7 +15,7 @@ public class SetBet extends PayBet{
         Craps.bankDelta -= bet;
     }
 
-
+boolean on;
     int bet;
     static boolean singleBetFlag;
 }
